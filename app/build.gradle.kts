243fc9ef2949c5358a0f0178c7a76d236cff2c54
DannyLien <danny.lien1109@gmail.com>
2025-12-24 23:22:56 +0800

    Gradle, Firebase connect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,13 +41,41 @@ android {
 }
 
 dependencies {
-
+    // FireDatabase :: Auth , RealTime , UI
+    // https://firebase.google.com/docs/android/setup?hl=zh-tw
+    implementation("com.google.firebase:firebase-auth:23.2.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")		//kotlin 8.0.2 //java 7.2.0
+    implementation("com.firebaseui:firebase-ui-database:8.0.2")	//kotlin 8.0.2 //java 7.2.0
+    implementation("com.google.gms:google-services:4.4.2")
+    //
+    //coil
+    implementation("io.coil-kt:coil:2.6.0")
+    //Okhttp
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    //ROOM
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+    //ViewModel, LiveData
+    val lifecycle_version = "2.7.0"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    // Annotation processor
+    kapt("androidx.lifecycle:lifecycle-compiler:$lifecycle_version")
+    // init
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
